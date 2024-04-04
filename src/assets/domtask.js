6653017a9d0cd0e643e0c2a89108f795eb20bbb1
Ioannis Boufogiannis <ioannisboufogiannis@gmail.com>
2024-04-04 20:38:16 +0300

    more UI fixes , left side mostly

--- src/assets/domtask.js
+++ src/assets/domtask.js
@@ -4,8 +4,13 @@ export function loadRight(project){
     const project_header=document.querySelector('#project_header');
     const content=document.querySelector('#content');
 
-    project_header.textContent='';
-    project_header.textContent=project["title"];
+    project_header.innerHTML='';
+    project_header.innerHTML=`
+    <div id="head_icon">
+      <p>${project["title"]}</p>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>invoice-list-outline</title><path d="M17 7V9H15V7H17M13 7V9H7V7H13M13 11H7V13H13V11M15 11V13H17V11H15M21 22L18 20L15 22L12 20L9 22L6 20L3 22V3H21V22M19 18.26V5H5V18.26L6 17.6L9 19.6L12 17.6L15 19.6L18 17.6L19 18.26Z" /></svg>    </div>
+    `
+    // project_header.textContent=`~${project["title"]}`;
 
     content.innerHTML='';
     content.innerHTML=`
